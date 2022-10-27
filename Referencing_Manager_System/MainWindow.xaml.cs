89645429e6c26d3b60ce765fa76cacd59d6e55eb
ChloeMoodley <ST10119385@vcconnect.edu.za>
2022-10-28 00:33:10 +0200

    Home Page UI

---
+++ Referencing_Manager_System/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Referencing_Manager_System
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void begin_Click(object sender, RoutedEventArgs e)
+        {
+/*            LoginPage LP = new LoginPage();
+            this.Close();
+            LP.Show();*/
+        }
+
+        private void register_Click(object sender, RoutedEventArgs e)
+        {
+            /*RegisterPage RP = new RegisterPage();
+            this.Close();
+            RP.Show();*/
+        }
+    }
+}
